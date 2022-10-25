36dae56837a4a2deb4a39e497bd6fc5f503809e6
reed.1848 <reed.1848@buckeyemail.osu.edu>
2022-10-25 12:54:05 -0400

    Added IHUD and IHUDElement Interfaces.

---
+++ Zelda/HUD/IHUD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zelda.HUD
+{
+    internal interface IHUD
+    {
+        public void Update();
+        public void Draw();
+    }
+}

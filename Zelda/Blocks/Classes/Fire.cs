1bf6c829ba2e5f7971ac8877407b4f4d9478eb47
jpohlabel <jacobapohlabel5@gmail.com>
2022-10-31 11:36:56 -0400

    Fixed .gitignore issue

---
+++ Zelda/Blocks/Classes/Fire.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Zelda.Sprites.Factories;
+
+namespace Zelda.Blocks.Classes
+{
+    public class Fire : IBlock
+    {
+        public Fire(Vector2 position) : base(BlockSpriteFactory.FireSprite(), position, false, true)
+        {
+
+        }
+    }
+}
+
---
+++ Zelda/Rooms/Parsers/AdjacentRoomParser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Zelda.Borders;
+using Zelda.Items;
+using Zelda.Items.Classes;
+
+namespace Zelda.Rooms.Parsers
+{
+    public class AdjacentRoomParser : Parser
+    {
+        private Dictionary<Room.Direction, Room> adjacentRooms;
+        private RoomBuilder roomBuilder;
+
+        public AdjacentRoomParser(Room room, Dictionary<Room.Direction, Room> adjacentRooms, RoomBuilder roomBuilder) 
+            : base(room, "..\\..\\..\\Rooms\\Files\\" + room.Name + "\\adjacent.csv")
+        {
+            this.adjacentRooms = adjacentRooms;
+            this.roomBuilder = roomBuilder;
+        }
+
+        protected override void ParseObject(string identifier, int i, int j)
+        {
+            Room.Direction direction;
+            if (i == 0)
+                direction = Room.Direction.Left;
+            else if (i == 1)
+                direction = Room.Direction.Right;
+            else if (i == 2)
+                direction = Room.Direction.Top;
+            else
+                direction = Room.Direction.Bottom;
+            adjacentRooms.Add(direction, roomBuilder.GetRoom(identifier));
+        }
+    }
+}

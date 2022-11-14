a85779766233615d74a43cbeca474d0574f439f5
reed.1848 <reed.1848@buckeyemail.osu.edu>
2022-11-14 14:40:42 -0500

    Fix Font.

--- Zelda/Inventory/InventoryBuidler.cs
+++ Zelda/Inventory/InventoryBuidler.cs
@@ -14,7 +14,7 @@ namespace Zelda.Inventory
         public static void BuildInventory(IInventory inventory)
         {
             inventory.AddItem(new Bomb(new Vector2(0, 0)), 1); // Max Count is 8
-            inventory.AddItem(new Sword(new Vector2(0, 0)), 1); // For Sword and SwordBeam
+            inventory.AddItem(new Sword(new Vector2(0, 0)), 999); // For Sword and SwordBeam
             inventory.AddItem(new Arrow(new Vector2(0, 0)), 1); // Need Bow and 1 Rupee cost
             inventory.AddItem(new Boomerang(new Vector2(0, 0)), 1); // Infinite
             inventory.AddItem(new BlueCandle(new Vector2(0, 0)), 1); // For CandleFlame (Blue can be used once per screen) red infinite

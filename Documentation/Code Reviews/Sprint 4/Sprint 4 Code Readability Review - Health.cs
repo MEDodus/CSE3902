3f672b79b7df1045e1e12d7d6bc7f93ffe675239
MEDodus <96406070+MEDodus@users.noreply.github.com>
2022-11-14 23:20:52 -0500

    added code reviews

---
+++ Documentation/Code Reviews/Sprint 4/Sprint 4 Code Readability Review - Health.cs
@@ -0,0 +1,16 @@
+Review By: Michael Dodus
+Date: 11 / 14 / 2022
+Sprint #: 4
+Class Reviewed: Health.cs
+Author: Nicholas Younoszai
+Time: 20 mins
+
+Code Readability:
+This class is relatively short, especially in term of class member variables.All the methods function as intented and has descriptive names describing
+their purpose. Each method relies on one another for reusability, not code is written more than once. The Health class is fairly straightforward just dealing 
+with an int variable to keep track of link's health not much more to say on it. I personally am a strong advocate for inline code snippets, I use them quite a
+bit so they have gotten easier with time, so if I were to have it my way I'd say use more... though I know most people would probably advise against this haha.
+
+Hypothetical Change:
+Change all code to one lines and surpass all programmers.
+
